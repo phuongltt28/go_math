3a90a2a931495a033babb63f3accd85fbbc8043c
Phuong Le Thi Truc <55975124+phuongltt28@users.noreply.github.com>
2023-01-30 18:23:45 +0700

    Lecture 200-204

---
+++ v2/calc/calc.go
@@ -0,0 +1,13 @@
+package calc
+
+func Add(args ...int) int {
+	s := 0
+	for _, v := range a {
+		s += v
+	}
+	return s
+}
+
+func Sub(a, b int) int {
+	return a - b
+}
---
+++ v2/go.mod
@@ -0,0 +1,3 @@
+module github.dxc.com/ple47/go_math/v2
+
+go 1.19
